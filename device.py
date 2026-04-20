63e57b0ec8502b8bea5b6f23466c53ee85465778
daniel lin <daniellin139@gmail.com>
2026-04-19 19:05:24 -0700

    add new function to support new workflow

--- device.py
+++ device.py
@@ -23,7 +23,7 @@ def get_interface_optic_detail(device_name, interface_name):
         'ethernet1/2': {'state':'bad', 'rx': '-30', 'tx': '1.10'}
     }
     if interface_name.lower() in optic_detail:
-        return (True, {'state':optic_detail['interface_name']['state'], 'interface_name': optic_detail[interface_name]})
+        return (True, {'state':optic_detail[interface_name]['state'], 'interface_name': optic_detail[interface_name]})
     else:
         return (False, {'error': f'Device {device_name} interface {interface_name} optic detail not available.'})
     
@@ -38,6 +38,51 @@ def check_interface_optic_light_level(device_name, interface_name):
         'ethernet1/2': {'state':'good', 'rx': '0.89', 'tx': '1.10'}
     }
     if interface_name.lower() in optic_detail:
-        return (True, {'state':optic_detail['interface_name']['state'], 'interface_name': optic_detail[interface_name]})
+        return (True, {'state':optic_detail[interface_name]['state'], 'interface_name': optic_detail[interface_name]})
     else:
         return (False, {'error': f'Device {device_name} interface {interface_name} optic detail not available.'})
+
+
+def set_silence_the_device_and_neighbors(device_name):
+    return (True, {'message':f'silense set for device {device_name}'})
+
+
+def collect_snapshot(device_name):
+    return (True, {'message':f'snapshot collected from device {device_name}.'})
+
+
+def get_traffic_drain_config_and_apply(device_name):
+    return (True, {'message':f'traffic drain config applied to device {device_name}.'})
+
+
+def check_if_traffic_goes_away(device_name):
+    return (True, {'message':f'Device {device_name}' traffic went away.})
+
+
+def upgrade_eos_image(device_name, target_eos_version):
+    return (True, {'message':f'Device {device_name} has been upgraded to version {v}'})
+
+
+def print_console_info(device_name):
+    return (True, {'message':f'Device {device_name}'s console info: ts1-chq2b0140, port01, please login the console to monitor the upgrade process.'})
+
+
+def collect_snapshot_and_compare_previous_snapshot(device_name):
+    return (True, {'message':f'Device {device_name} snapshot look good before upgrade and after upgrade.'})
+
+
+def get_traffic_undrain_config_and_apply(device_name):
+    return (True, {'message':f'Device {device_name} has been applied undrain config.'})
+
+
+def check_if_traffic_comes_back(device_name):
+    return (True, {'message':f'Traffic is coming back, Device {device_name} is taking traffic.'})
+
+
+def clear_alerts(device_name):
+    return (True, {'message':f"Cleared the alerts for the device {device_name} and it's neighbors"})
+
+
+def expire_silence(device_name):
+    return (True, {'message':f'expired the silence for device {device_name}'})
+
